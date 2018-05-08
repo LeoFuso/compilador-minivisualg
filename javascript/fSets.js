2f44fc453e71581c819a9bfff7e78173ad2fdabb
gmfc <gabriel.mfcorreia@gmail.com>
2018-05-08 15:47:16 -0300

    fix config IDE js

--- javascript/fSets.js
+++ javascript/fSets.js
@@ -1,8 +1,7 @@
-// Special "empty" symbol.
-var EPSILON = "ε";
+let EPSILON = 'ε'
 
-var firstSets = {};
-var followSets = {};
+var firstSets = {}
+var followSets = {}
 
 /**
  * Rules for First Sets
@@ -18,51 +17,50 @@ var followSets = {};
  *       to First(Y1Y2..Yk) as well.
  */
 
-function buildFirstSets(grammar) {
-  firstSets = {};
-  buildSet(firstOf);
+function buildFirstSets (grammar) {
+  firstSets = {}
+  buildSet(firstOf)
 }
 
-function firstOf(symbol) {
-
+function firstOf (symbol) {
   // A set may already be built from some previous analysis
   // of a RHS, so check whether it's already there and don't rebuild.
   if (firstSets[symbol]) {
-    return firstSets[symbol];
+    return firstSets[symbol]
   }
 
   // Else init and calculate.
-  var first = firstSets[symbol] = {};
+  var first = firstSets[symbol] = {}
 
   // If it's a terminal, its first set is just itself.
   if (isTerminal(symbol)) {
-    first[symbol] = true;
-    return firstSets[symbol];
+    first[symbol] = true
+    return firstSets[symbol]
   }
 
-  var productionsForSymbol = getProductionsForSymbol(symbol);
+  var productionsForSymbol = getProductionsForSymbol(symbol)
   for (var k in productionsForSymbol) {
-    var production = getRHS(productionsForSymbol[k]);
+    var production = getRHS(productionsForSymbol[k])
 
     for (var i = 0; i < production.length; i++) {
-      var productionSymbol = production[i];
+      var productionSymbol = production[i]
 
       // Epsilon goes to the first set.
       if (productionSymbol === EPSILON) {
-        first[EPSILON] = true;
-        break;
+        first[EPSILON] = true
+        break
       }
 
       // Else, the first is a non-terminal,
       // then first of it goes to first of our symbol
       // (unless it's an epsilon).
-      var firstOfNonTerminal = firstOf(productionSymbol);
+      var firstOfNonTerminal = firstOf(productionSymbol)
 
       // If first non-terminal of the RHS production doesn't
       // contain epsilon, then just merge its set with ours.
       if (!firstOfNonTerminal[EPSILON]) {
-        merge(first, firstOfNonTerminal);
-        break;
+        merge(first, firstOfNonTerminal)
+        break
       }
 
       // Else (we got epsilon in the first non-terminal),
@@ -70,12 +68,12 @@ function firstOf(symbol) {
       //   - merge all except for epsilon
       //   - eliminate this non-terminal and advance to the next symbol
       //     (i.e. don't break this loop)
-      merge(first, firstOfNonTerminal, [EPSILON]);
+      merge(first, firstOfNonTerminal, [EPSILON])
       // don't break, go to the next `productionSymbol`.
     }
   }
 
-  return first;
+  return first
 }
 
 /**
@@ -90,29 +88,29 @@ function firstOf(symbol) {
  * Given symbol `S`, the function returns `S -> F`,
  * and `S -> (S + F)` productions.
  */
-function getProductionsForSymbol(symbol) {
-  var productionsForSymbol = {};
+function getProductionsForSymbol (symbol) {
+  var productionsForSymbol = {}
   for (var k in grammar) {
     if (grammar[k][0] === symbol) {
-      productionsForSymbol[k] = grammar[k];
+      productionsForSymbol[k] = grammar[k]
     }
   }
-  return productionsForSymbol;
+  return productionsForSymbol
 }
 
 /**
  * Given production `S -> F`, returns `S`.
  */
-function getLHS(production) {
-  //return production.split('->')[0].replace(/\s+/g, '');
+function getLHS (production) {
+  // return production.split('->')[0].replace(/\s+/g, '');
   return production[0]
 }
 
 /**
  * Given production `S -> F`, returns `F`.
  */
-function getRHS(production) {
-  //return production.split('->')[1].replace(/\s+/g, '');
+function getRHS (production) {
+  // return production.split('->')[1].replace(/\s+/g, '');
   return production.slice(2)
 }
 
@@ -128,37 +126,36 @@ function getRHS(production) {
  *   then everything in FOLLOW(A) is in FOLLOW(B)
  */
 
-function buildFollowSets(grammar) {
-  followSets = {};
-  buildSet(followOf);
+function buildFollowSets (grammar) {
+  followSets = {}
+  buildSet(followOf)
 }
 
-function followOf(symbol) {
-
+function followOf (symbol) {
   // If was already calculated from some previous run.
   if (followSets[symbol]) {
-    return followSets[symbol];
+    return followSets[symbol]
   }
 
   // Else init and calculate.
-  var follow = followSets[symbol] = {};
+  var follow = followSets[symbol] = {}
 
   // Start symbol always contain `$` in its follow set.
-  //console.log(symbol + '<<')
+  // console.log(symbol + '<<')
   if (symbol === START_SYMBOL) {
-    follow['$'] = true;
+    follow['$'] = true
   }
 
   // We need to analyze all productions where our
   // symbol is used (i.e. where it appears on RHS).
-  var productionsWithSymbol = getProductionsWithSymbol(symbol);
+  var productionsWithSymbol = getProductionsWithSymbol(symbol)
   for (var k in productionsWithSymbol) {
-    var production = productionsWithSymbol[k];
-    var RHS = getRHS(production);
+    var production = productionsWithSymbol[k]
+    var RHS = getRHS(production)
 
     // Get the follow symbol of our symbol.
-    var symbolIndex = RHS.indexOf(symbol);
-    var followIndex = symbolIndex + 1;
+    var symbolIndex = RHS.indexOf(symbol)
+    var followIndex = symbolIndex + 1
 
     // We need to get the following symbol, which can be `$` or
     // may contain epsilon in its first set. If it contains epsilon, then
@@ -167,38 +164,37 @@ function followOf(symbol) {
     // as the follow of `B`.
 
     while (true) {
-
       if (followIndex === RHS.length) { // "$"
-        var LHS = getLHS(production);
+        var LHS = getLHS(production)
         if (LHS !== symbol) { // To avoid cases like: B -> aB
-          merge(follow, followOf(LHS));
+          merge(follow, followOf(LHS))
         }
-        break;
+        break
       }
 
-      var followSymbol = RHS[followIndex];
+      var followSymbol = RHS[followIndex]
 
       // Follow of our symbol is anything in the first of the following symbol:
       // followOf(symbol) is firstOf(followSymbol), except for epsilon.
-      var firstOfFollow = firstOf(followSymbol);
+      var firstOfFollow = firstOf(followSymbol)
 
       // If there is no epsilon, just merge.
       if (!firstOfFollow[EPSILON]) {
-        merge(follow, firstOfFollow);
-        break;
+        merge(follow, firstOfFollow)
+        break
       }
 
-      merge(follow, firstOfFollow, [EPSILON]);
-      followIndex++;
+      merge(follow, firstOfFollow, [EPSILON])
+      followIndex++
     }
   }
 
-  return follow;
+  return follow
 }
 
-function buildSet(builder) {
+function buildSet (builder) {
   for (var k in grammar) {
-    builder(grammar[k][0]);
+    builder(grammar[k][0])
   }
 }
 
@@ -207,49 +203,49 @@ function buildSet(builder) {
  * symbol `S` it finds production `(S + F)`, and for the symbol `F`
  * it finds productions `F` and `(S + F)`.
  */
-function getProductionsWithSymbol(symbol) {
-  var productionsWithSymbol = {};
+function getProductionsWithSymbol (symbol) {
+  var productionsWithSymbol = {}
   for (var k in grammar) {
-    var production = grammar[k];
-    var RHS = getRHS(production);
+    var production = grammar[k]
+    var RHS = getRHS(production)
     if (RHS.indexOf(symbol) !== -1) {
-      productionsWithSymbol[k] = production;
+      productionsWithSymbol[k] = production
     }
   }
-  return productionsWithSymbol;
+  return productionsWithSymbol
 }
 
-function isTerminal(symbol) {
-  return !/[A-Z]/.test(symbol);
+function isTerminal (symbol) {
+  return !/[A-Z]/.test(symbol)
 }
 
-function merge(to, from, exclude) {
-  exclude || (exclude = []);
+function merge (to, from, exclude) {
+  exclude || (exclude = [])
   for (var k in from) {
     if (exclude.indexOf(k) === -1) {
-      to[k] = from[k];
+      to[k] = from[k]
     }
   }
 }
 
-function printGrammar(grammar) {
-  console.log('Grammar:\n');
+function printGrammar (grammar) {
+  console.log('Grammar:\n')
   for (var k in grammar) {
-    console.log('  ', grammar[k]);
+    console.log('  ', grammar[k])
   }
-  console.log('');
+  console.log('')
   console.log(JSON.stringify(grammar))
-  console.log('--');
+  console.log('--')
 }
 
-function printSet(name, set) {
-  console.log(name + ': \n');
+function printSet (name, set) {
+  console.log(name + ': \n')
   for (var k in set) {
-    console.log('  ', k, ':', Object.keys(set[k]));
+    console.log('  ', k, ':', Object.keys(set[k]))
   }
-  console.log('');
+  console.log('')
   console.log(JSON.stringify(set))
-  console.log('--');
+  console.log('--')
 }
 
 // Testing
@@ -263,8 +259,7 @@ var grammar = {
   1: ['S', '->', 'F'],
   2: ['S', '->', '(', 'S', '+', 'F', ')'],
   3: ['F', '->', 'a'],
-};*/
-
+}; */
 
 var grammar = {
   1: ['E', '->', 'T', 'X'],
@@ -274,15 +269,15 @@ var grammar = {
   5: ['Y', '->', '*', 'F', 'Y'],
   6: ['Y', '->', 'ε'],
   7: ['F', '->', 'a'],
-  8: ['F', '->', '(', 'E', ')'],
-};
+  8: ['F', '->', '(', 'E', ')']
+}
 
-var START_SYMBOL = 'E';
+var START_SYMBOL = 'E'
 
-printGrammar(grammar);
+printGrammar(grammar)
 
-buildFirstSets(grammar);
-printSet('First sets', firstSets);
+buildFirstSets(grammar)
+printSet('First sets', firstSets)
 
-buildFollowSets(grammar);
-printSet('Follow sets', followSets);
+buildFollowSets(grammar)
+printSet('Follow sets', followSets)
--- javascript/table.js
+++ javascript/table.js
@@ -1,78 +1,77 @@
 // Special "empty" symbol.
-var EPSILON = 'ε';
+var EPSILON = 'ε'
 
 /**
  * Given a grammar builds a LL(1) parsing table based on the
  * First and Follow sets of this grammar.
  */
-function buildParsingTable(grammar) {
-	var parsingTable = {};
+function buildParsingTable (grammar) {
+  var parsingTable = {}
 
-	for (var k in grammar) {
-		var production = grammar[k];
-		var LHS = getLHS(production);
-		var RHS = getRHS(production);
-		var productionNumber = Number(k);
+  for (var k in grammar) {
+    var production = grammar[k]
+    var LHS = getLHS(production)
+    var RHS = getRHS(production)
+    var productionNumber = Number(k)
 
-		// Init columns for this non-terminal.
-		if (!parsingTable[LHS]) {
-			parsingTable[LHS] = {};
-		}
+    // Init columns for this non-terminal.
+    if (!parsingTable[LHS]) {
+      parsingTable[LHS] = {}
+    }
 
-		// All productions goes under the terminal column, if
-		// this terminal is not epsilon.
-		if (RHS[0] !== EPSILON) {
-			var tttt = getFirstSetOfRHS(RHS);
-			tttt.forEach(function (terminal) {
-				parsingTable[LHS][terminal] = productionNumber;
-			});
-		} else {
-			// Otherwise, this ε-production goes under the columns from
-			// the Follow set.
-			followSets[LHS].forEach(function (terminal) {
-				parsingTable[LHS][terminal] = productionNumber;
-			});
-		}
-	}
+    // All productions goes under the terminal column, if
+    // this terminal is not epsilon.
+    if (RHS[0] !== EPSILON) {
+      var tttt = getFirstSetOfRHS(RHS)
+      tttt.forEach(function (terminal) {
+        parsingTable[LHS][terminal] = productionNumber
+      })
+    } else {
+      // Otherwise, this ε-production goes under the columns from
+      // the Follow set.
+      followSets[LHS].forEach(function (terminal) {
+        parsingTable[LHS][terminal] = productionNumber
+      })
+    }
+  }
 
-	return parsingTable;
+  return parsingTable
 }
 
 /**
  * Given production `S -> F`, returns `S`.
  */
-function getLHS(production) {
-	// return production.split('->')[0].replace(/\s+/g, '');
-	return production[0]
+function getLHS (production) {
+  // return production.split('->')[0].replace(/\s+/g, '');
+  return production[0]
 }
 
 /**
  * Given production `S -> F`, returns `F`.
  */
-function getRHS(production) {
-	// return production.split('->')[1].replace(/\s+/g, '');
-	return production.slice(2)
+function getRHS (production) {
+  // return production.split('->')[1].replace(/\s+/g, '');
+  return production.slice(2)
 }
 
 /**
  * Returns First set of RHS.
  */
-function getFirstSetOfRHS(RHS) {
+function getFirstSetOfRHS (RHS) {
+  // For simplicity, in this educational parser, we assume that
+  // the first symbol (if it's a non-terminal) cannot produces `ε`.
+  // Since in real parser, we need to get the First set of the whole RHS.
+  // This means, that if `B` in the production `X -> BC` can be `ε`, then
+  // the First set should of course include First(C) as well, i.e. RHS[1], etc.
+  //
+  // That is, in a real parser, one usually combines steps of building a
+  // parsing table, First and Follow sets in one step: when a parsing table
+  // needs the First set of a RHS, it's calculated in place.
+  //
+  // But here we just return First of RHS[0].
+  //
 
-	// For simplicity, in this educational parser, we assume that
-	// the first symbol (if it's a non-terminal) cannot produces `ε`.
-	// Since in real parser, we need to get the First set of the whole RHS.
-	// This means, that if `B` in the production `X -> BC` can be `ε`, then
-	// the First set should of course include First(C) as well, i.e. RHS[1], etc.
-	//
-	// That is, in a real parser, one usually combines steps of building a
-	// parsing table, First and Follow sets in one step: when a parsing table
-	// needs the First set of a RHS, it's calculated in place.
-	//
-	// But here we just return First of RHS[0].
-	//
-
-	return firstSets[RHS[0]];
+  return firstSets[RHS[0]]
 }
 
 // Testing
@@ -83,29 +82,29 @@ function getFirstSetOfRHS(RHS) {
 
 // We just manually define our First and Follow sets for a given grammar,
 // see again diff (2) where we automatically generated these sets.
-/*
+
 var grammar = {
-	1: ['S', '->', 'F'],
-	2: ['S', '->', '(', 'S', '+', 'F', ')'],
-	3: ['F', '->', 'a'],
-};
+  1: ['S', '->', 'F'],
+  2: ['S', '->', '(', 'S', '+', 'F', ')'],
+  3: ['F', '->', 'a']
+}
 
 // See https://gist.github.com/DmitrySoshnikov/924ceefb1784b30c5ca6
 // for the sets construction.
 
 var firstSets = {
-	'S' : [ 'a', '(' ],
-	'F' : [ 'a' ],
-	'a' : [ 'a' ],
-	'(' : [ '(' ]
-};
+  'S': ['a', '('],
+  'F': ['a'],
+  'a': ['a'],
+  '(': ['(']
+}
 
 var followSets = {
-	'S' : [ '$', '+' ],
-	'F' : [ '$', '+', ')' ]
-};
+  'S': ['$', '+'],
+  'F': ['$', '+', ')']
+}
 
-console.log(buildParsingTable(grammar));
+console.log(buildParsingTable(grammar))
 
 // Results:
 
@@ -120,46 +119,46 @@ console.log(buildParsingTable(grammar));
 // | S  2  -  1  -  - |
 // | F  -  -  3  -  - |
 // +------------------+
-*/
+
 // ----------------------------------------------------------------------
 // Example 2, for the "calculator" grammar, e.g. (a + a) * a.
 // ----------------------------------------------------------------------
-
+/*
 var grammar = {
-	1: ['E', '->', 'T', 'X'],
-	2: ['X', '->', '+', 'T', 'X'],
-	3: ['X', '->', 'ε'],
-	4: ['T', '->', 'F', 'Y'],
-	5: ['Y', '->', '*', 'F', 'Y'],
-	6: ['Y', '->', 'ε'],
-	7: ['F', '->', 'a'],
-	8: ['F', '->', '(', 'E', ')'],
-};
+  1: ['E', '->', 'T', 'X'],
+  2: ['X', '->', '+', 'T', 'X'],
+  3: ['X', '->', 'ε'],
+  4: ['T', '->', 'F', 'Y'],
+  5: ['Y', '->', '*', 'F', 'Y'],
+  6: ['Y', '->', 'ε'],
+  7: ['F', '->', 'a'],
+  8: ['F', '->', '(', 'E', ')']
+}
 
 // See https://gist.github.com/DmitrySoshnikov/924ceefb1784b30c5ca6
 // for the sets construction.
 
 var firstSets = {
-	'E': ['a', '('],
-	'T': ['a', '('],
-	'F': ['a', '('],
-	'a': ['a'],
-	'(': ['('],
-	'X': ['+', 'ε'],
-	'+': ['+'],
-	'Y': ['*', 'ε'],
-	'*': ['*'],
-};
+  'E': ['a', '('],
+  'T': ['a', '('],
+  'F': ['a', '('],
+  'a': ['a'],
+  '(': ['('],
+  'X': ['+', 'ε'],
+  '+': ['+'],
+  'Y': ['*', 'ε'],
+  '*': ['*']
+}
 
 var followSets = {
-	'E': ['$', ')'],
-	'X': ['$', ')'],
-	'T': ['+', '$', ')'],
-	'Y': ['+', '$', ')'],
-	'F': ['*', '+', '$', ')'],
-};
+  'E': ['$', ')'],
+  'X': ['$', ')'],
+  'T': ['+', '$', ')'],
+  'Y': ['+', '$', ')'],
+  'F': ['*', '+', '$', ')']
+}
 
-console.log(buildParsingTable(grammar));
+console.log(buildParsingTable(grammar))
 
 // Results:
 
@@ -180,3 +179,4 @@ console.log(buildParsingTable(grammar));
 // | Y  -  6  5  -  6  6 |
 // | F  7  -  -  8  -  - |
 // +---------------------+
+*/
