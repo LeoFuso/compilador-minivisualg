a6bb52abf7cf8207a09abbd5a809fc85a11b1144
gmfc <gabriel.mfcorreia@gmail.com>
2018-05-09 14:30:21 -0300

    followSets criados

---
+++ followSets.js
@@ -0,0 +1,17 @@
+var followSets = {
+	'A': ['$'],
+	'B': ['<inicio>'],
+	'D': ['<var>', '<id>', '<del|,>', '<del|:>', '<inicio>'],
+	'F': ['<var>', '<id>', '<del|,>', '<del|:>', '<inicio>'],
+	'E': ['<var>', '<id>', '<del|,>', '<del|:>', '<inicio>'],
+	'G': ['<del|:>', '<del|)>'],
+	'C': ['<fimalgoritmo>', '<senao>', '<fimpara>', '<fimenquanto>'],
+	'H': ['<id>', '<leia>', '<escreva>', '<se>', '<para>', '<enquanto>', '<fimalgoritmo>', '<senao>', '<fimpara>', '<fimenquanto>'],
+	'I': ['<id>', '<leia>', '<escreva>', '<se>', '<para>', '<enquanto>', '<fimalgoritmo>', '<senao>', '<fimpara>', '<fimenquanto>'],
+	'J': ['<del|)>'],
+	'K': ['<entao>', '<faca>'],
+	'L': ['<id>', '<leia>', '<escreva>', '<se>', '<para>', '<enquanto>', '<fimalgoritmo>', '<senao>', '<fimpara>', '<fimenquanto>', '<num>', '<str>', '<verdadeiro>', '<falso>', '<op|+>', '<op|->', '<op|*>', '<op|/>', '<op|mod>', '<op|exp>', '<lop|>>', '<lop|>=>', '<lop|<>', '<lop|<=>', '<lop|=>', '<lop|<>>', '<lop|e>', '<lop|ou>'],
+	'M': ['<id>', '<leia>', '<escreva>', '<se>', '<para>', '<enquanto>', '<fimalgoritmo>', '<senao>', '<fimpara>', '<fimenquanto>', '<lop|>>', '<lop|>=>', '<lop|<>', '<lop|<=>', '<lop|=>', '<lop|<>>', '<lop|e>', '<lop|ou>'],
+	'N': ['<id>', '<num>', '<str>', '<verdadeiro>', '<falso>', '<op|+>', '<op|->', '<op|*>', '<op|/>', '<op|mod>', '<op|exp>', '<lop|>>', '<lop|>=>', '<lop|<>', '<lop|<=>', '<lop|=>', '<lop|<>>', '<lop|e>', '<lop|ou>', '<entao>', '<faca>'],
+	'O': ['<id>', '<num>', '<str>', '<verdadeiro>', '<falso>']
+}
