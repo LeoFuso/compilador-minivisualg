fe8f17be0061d1501a26fa570fe48a09b15ec878
LeoFuso <leonardofusonuzzo@gmail.com>
2018-05-09 14:18:33 -0300

    a better signature for the operations

--- compilador/compiler.c
+++ compilador/compiler.c
@@ -1,5 +1,28 @@
 //
-// Created by lfuso on 09/05/18.
+// Created by Leonardo Fuso on 09/05/18.
 //
 
 #include "compiler.h"
+
+struct Token
+{
+    TokenType tokenType;
+    const char *value;
+    const char *body;
+    const char *source;
+};
+
+struct Line
+{
+    int line_address;
+    char *body;
+    unsigned numtkns;
+    struct Token **tokens;
+    int error;
+};
+
+struct Line *
+lexical_analysis(File *);
+
+int
+syntax_analysis(struct Line **);
--- compilador/compiler.h
+++ compilador/compiler.h
@@ -5,27 +5,7 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
-struct Token
-{
-    TokenType tokenType;
-    const char *value;
-    const char *body;
-    const char *source;
-};
-
-struct Line
-{
-    int line_address;
-    char *body;
-    unsigned numtkns;
-    struct Token **tokens;
-    int error;
-};
-
-struct Line *
-lexical_analysis(File *);
-
-int
-syntax_analysis(struct Line **);
+File *
+compile(char *);
 
 #endif //COMPILER_H
