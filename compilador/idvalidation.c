431780e9c8eaf0ef961412dbb34b461d060ac53f
gabriel.mfcorreia <gabriel.mfcorreia@gmail.com>
2018-05-27 17:04:27 -0400

    seg fault

--- compilador/idvalidation.c
+++ compilador/idvalidation.c
@@ -1,17 +1,57 @@
 #include "idvalidation.h"
 
+int declared[10];
+
 int
 id_validation(struct Line **program, unsigned int lncnt) {
+  int declaredOK = _chckDclrd(program,lncnt);
+}
+
+int
+_chckDclrd(struct Line **program, unsigned int lncnt) {
   int declaration_state = 0;
   for(int i=0; i<lncnt; ++i)
-    for(int j=0; j<program[i]->numtkns; ++j)
+    // printf(" >>%s:%s:%s<<\n", program[i]->tokens[j]->value, program[i]->tokens[j]->body, program[i]->tokens[j]->to_parse);
+    // Estamos listando vars declaradas
+    if(declaration_state == 0)
     {
-      if(_tknIddclrEnd(program[i]->tokens[j]) == 1)
+      // Se chegamos em inicio, nao estamos mais declarando. Mudar estado para 1
+      if(strcmp("<inicio>", program[i]->tokens[0]->body) == 1)
       {
-        int declaration_state = 1;
+        declaration_state = 1;
       }
-      printf(" >> %s <<\n", program[i]->tokens[j]->body);
+      else
+      {
+        // Se conseguimos ID como primeiro token, declaracao unica,
+        // recupera tipo e id
+        if(strcmp("<var>", program[i]->tokens[0]->body))
+        {
+          // int id = (int) program[i]->tokens[1]->value - "0"; // VOODOOoooo.....
+          int id = (int) strtol(program[i]->tokens[1]->value, (char **)NULL, 10);
+          int type = strcmp("<inteiro>",program[i]->tokens[3]->body); // 1 se for int, 0 se for bool
+          declared[id] = type;
+        }
+        // multivar declare, mais dificil...
+        else
+        {
+          for(int j=0; j<program[i]->numtkns;++j)
+          {
+            if(strcmp("<id>", program[i]->tokens[3]->to_parse) == 1)
+            {
+              int id = (int) strtol(program[i]->tokens[j]->value, (char **)NULL, 10);
+              int type = strcmp("<inteiro>",program[i]->tokens[program[i]->numtkns-1]->body);
+              declared[id] = type;
+            }
+          }
+        }
+      }
     }
+    
+    for(int i=0; i<10; ++i)
+    {
+      printf("%d \n", declared[i]);
+    }
+    
   return 1;
 }
 
